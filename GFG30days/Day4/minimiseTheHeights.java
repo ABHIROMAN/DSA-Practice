ac3cc22698f57166bb25ebc77df2c462afd2cb82
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2022-11-09 23:00:52 +0530

    Updating homewrok part

---
+++ GFG30days/Day4/minimiseTheHeights.java
@@ -0,0 +1,26 @@
+package GFG30days.Day4;
+
+import java.util.*;
+
+public class minimiseTheHeights {
+    
+    int getMinDiff(int arr[], int n, int k) {
+
+        if(n == 1) return 0;
+
+        Arrays.sort(arr);
+
+        int diff = arr[n-1] - arr[0];
+        int min, max;
+
+        for(int i = 1; i < n; i++) {
+
+            if(arr[i] - k < 0) continue;
+            max = Math.max(arr[i-1]+k, arr[n-1]-k);
+            min = Math.min(arr[0]+k, arr[i]-k);
+
+            diff = Math.min(diff, max - min);
+        }
+        return diff;
+    }
+}
