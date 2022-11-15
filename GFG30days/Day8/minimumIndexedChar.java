0161cce06870f0c172b44462aa248fc937e0911c
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2022-11-15 20:02:15 +0530

    update minIndexchar

---
+++ GFG30days/Day8/minimumIndexedChar.java
@@ -0,0 +1,26 @@
+package GFG30days.Day8;
+
+import java.util.*;
+
+public class minimumIndexedChar {
+
+    public static int minIndexChar(String str, String patt) {
+
+        int mn = 100001;
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < patt.length(); i++) {
+
+            map.put(patt.charAt(i), i);
+        }
+
+        for(int j = 0; j < str.length(); j++) {
+
+            if(map.containsKey(str.charAt(j))) {
+                return j;
+            }
+        }
+        return -1;
+    }
+}
