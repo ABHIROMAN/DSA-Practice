abfa8b729210fade55a1441b48bf04650587afdf
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2022-11-17 22:47:37 +0530

    updating problem of the day

---
+++ GFG30days/Day11/countOfSubarrays.java
@@ -0,0 +1,23 @@
+package GFG30days.Day11;
+
+public class countOfSubarrays {
+
+    long countSubarray(int arr[], int n, int k) {
+
+        long ans = 0;
+        
+        for(int i = 0; i < n; i++) {
+            
+            for(int j = i; j < n; j++) {
+                
+                if(arr[j] > k) {
+                    
+                    ans += n - j;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+    
+}
