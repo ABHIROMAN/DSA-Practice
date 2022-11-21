2161c40264092e126ce989d4dfc49ede72f79f35
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2022-11-21 20:51:55 +0530

    updating last question

---
+++ GFG30days/Day14/kthSmallestElementInMatrix.java
@@ -0,0 +1,23 @@
+package GFG30days.Day14;
+
+import java.util.*;
+
+public class kthSmallestElementInMatrix {
+    
+    public static int kthSmallest(int[][]mat,int n,int k) {
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int ans = 0;
+        for(int i =0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                pq.add(mat[i][j]);
+            }
+        }
+        int start = 0;
+        while(start<k){
+            ans = pq.poll();
+            start++;
+        }
+        return ans;
+    }
+}
