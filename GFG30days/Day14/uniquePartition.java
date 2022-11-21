b11a79053b54773078eaf564e3d5f709847868d0
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2022-11-21 21:00:42 +0530

    updating Problem of the day

---
+++ GFG30days/Day14/uniquePartition.java
@@ -0,0 +1,38 @@
+package GFG30days.Day14;
+
+import java.util.*;
+
+public class uniquePartition {
+    
+    void dfs(ArrayList<ArrayList<Integer>>ans, ArrayList<Integer>curr, int idx, int n, int arr[]) {
+        
+        if(idx < 0 || n < 0) {
+            return;
+        }
+        
+        curr.add(arr[idx]);
+        if(n-arr[idx] == 0) {
+            
+            ans.add(new ArrayList<>(curr));
+        }
+        dfs(ans, curr, idx, n-arr[idx], arr);
+        curr.remove(curr.size()-1);
+        dfs(ans, curr, idx-1, n, arr);
+    }
+    
+    public ArrayList<ArrayList<Integer>> UniquePartitions(int n) {
+        
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        ArrayList<Integer> curr = new ArrayList<>();
+        
+        int idx = n - 1;
+        int arr[] = new int[n];
+        
+        for(int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
+        
+        dfs(res, curr, idx, n, arr);
+        return res;
+    }
+}
