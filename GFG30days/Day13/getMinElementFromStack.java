d502b958ad3cb7ab435afea7e064c73eec627c1e
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2022-11-21 13:35:28 +0530

    minStack pushing

---
+++ GFG30days/Day13/getMinElementFromStack.java
@@ -0,0 +1,37 @@
+package GFG30days.Day13;
+
+import java.util.*;
+
+public class getMinElementFromStack {
+
+    int minEle;
+    Stack<Integer> s = new Stack<>();
+
+    /*returns min element from stack*/
+    int getMin() {
+	// Your code here
+	    if(s.isEmpty()) return -1;
+	    minEle = s.peek();
+	    for(int i = 0; i<s.size(); i++) {
+	        if(minEle > s.get(i)) {
+	            minEle = s.get(i);
+	        }
+	    }
+	    return minEle;
+    }
+    
+    /*returns poped element from stack*/
+    int pop() {
+	// Your code here
+	    if(s.isEmpty()) return -1;
+	    
+	    return s.pop();
+    }
+
+    /*push element x into the stack*/
+    void push(int x) {
+	// Your code here
+	    s.push(x);
+    }
+    
+}
