c0c72c4d6394ee9f5c5abae0c6e3af564c34f373
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2022-11-26 16:28:04 +0530

    change name

---
+++ GFG30days/Day19/longestPalindromeInString.java
@@ -0,0 +1,44 @@
+package GFG30days.Day19;
+
+public class longestPalindromeInString {
+    
+    static String longestPalin(String S) {
+
+        int n = S.length();
+
+        String res = S.charAt(0) +  "";
+        int max = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            //Odd case 
+
+            int l = i, r = i;
+
+            while(l >= 0 && r < n && S.charAt(l) == S.charAt(r)) {
+
+                if(r - l > max) {
+                    res = S.substring(l, r+1);
+                    max = r - l;
+                }
+                r++;
+                l--;
+            }
+
+            //Even case
+            l = i;
+            r = i + 1;
+
+            while(l >= 0 && r < n && S.charAt(l) == S.charAt(r)) {
+
+                if(r - l > max) {
+                    res = S.substring(l, r+1);
+                    max = r - l;
+                }
+                r++;
+                l--;
+            }
+        }
+        return res;
+    }
+}
