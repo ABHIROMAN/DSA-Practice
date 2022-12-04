fcdbe7c0f2a44420776c849ecff4208d6593ca43
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2022-12-04 23:01:16 +0530

    updating reversing Linked lIst

---
+++ GFG30days/Day22/reverseALinkedList.java
@@ -0,0 +1,44 @@
+package GFG30days.Day22;
+
+class Node {
+
+    int data;
+    Node next;
+    Node(int key) {
+
+        data = key;
+        next = null;
+    }
+}
+
+public class reverseALinkedList {
+    
+    public static Node reverse(Node node, int k) {
+
+        if(node == null || node.next == null) {
+            return node;
+        }
+
+        Node prev = null;
+        Node curr = node;
+
+        Node start = node;
+        Node head = null;
+
+        Node next = null;
+        int c = k;
+
+        while(curr != null) {
+
+            if(k == 0) break;
+            k--;
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        node.next = reverse(curr, c);
+
+        return prev;
+    }
+}
