2fcdfc2a9fa7b37b679e89531a644f58b9280874
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2022-12-05 13:56:12 +0530

    updating LCA

---
+++ GFG30days/Day23/lowestCommonAncestor.java
@@ -0,0 +1,20 @@
+package GFG30days.Day23;
+
+public class lowestCommonAncestor {
+    
+    Node LCA(Node root, int n1, int n2) {
+        
+        if(root == null) return null;
+        
+        //check n1 and n2 are in left subtree
+        if(root.data > n1 && root.data > n2) {
+            return LCA(root.left, n1, n2);
+        }
+        //check n1 and n2 are in right subtree
+        if(root.data < n1 && root.data < n2) {
+            return LCA(root.right, n1, n2);
+        }
+        //else prsent node will be the LCA
+        return root;
+    }
+}
