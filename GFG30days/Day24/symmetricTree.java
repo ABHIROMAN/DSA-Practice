91dd314f8d07b3b6ae819a7479c7a7c6ab841e93
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2022-12-06 23:34:00 +0530

    updating symmetric tree

---
+++ GFG30days/Day24/symmetricTree.java
@@ -0,0 +1,26 @@
+package GFG30days.Day24;
+
+public class symmetricTree {
+    
+    static boolean isSafe(Node node1, Node node2) {
+        
+        if(node1 == null && node2 == null) return true;
+        
+        if(node1 == null || node2 == null) return false;
+        
+        if(node1.data != node2.data) {
+            
+            return false;
+        }
+        
+        return isSafe(node1.left, node2.right) && isSafe(node1.right, node2.left);
+    }
+    
+    // return true/false denoting whether the tree is Symmetric or not
+    public static boolean isSymmetric(Node root) {
+        
+        if(root == null) return true;
+        
+        return isSafe(root.left, root.right);
+    }
+}
