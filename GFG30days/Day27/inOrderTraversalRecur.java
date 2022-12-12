154a52cfe857cf6fcc9d54b2ad21bc010169bfbe
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2022-12-12 20:46:16 +0530

    updating inorder recur

---
+++ GFG30days/Day27/inOrderTraversalRecur.java
@@ -0,0 +1,32 @@
+package GFG30days.Day27;
+
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+   Node(int item)    {
+        data = item;
+        left = right = null;
+    }
+}
+public class inOrderTraversalRecur {
+    
+    ArrayList<Integer> inOrder(Node root) {
+
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        if(root == null) return arr;
+
+        inOrder(root, arr);
+        return arr;
+    }
+
+    public void inOrder(Node root, ArrayList<Integer> arr) {
+
+        if(root == null) return;
+        inOrder(root.left, arr);
+        arr.add(root.data);
+        inOrder(root.right, arr);
+    }
+
+}
