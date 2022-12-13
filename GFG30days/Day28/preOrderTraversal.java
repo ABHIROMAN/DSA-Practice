e26cb7f50e197cc46137ec56a6efadcd178a1f8d
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2022-12-13 18:43:51 +0530

    updating pre Order

---
+++ GFG30days/Day28/preOrderTraversal.java
@@ -0,0 +1,38 @@
+package GFG30days.Day28;
+
+import java.util.*;
+
+class Node{
+        int data;
+        Node left,right;
+        Node(int d){
+            data=d;
+            left=right=null;
+        }
+    }
+
+public class preOrderTraversal {
+    
+    static ArrayList<Integer> preorder(Node root) {
+        // Code here
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        Stack<Node> st = new Stack();
+        
+        st.push(root);
+        
+        while(!st.empty()) {
+            
+            Node temp = st.pop();
+            ans.add(temp.data);
+            
+            if(temp.right != null) {
+                st.push(temp.right);
+            }
+            if(temp.left != null) {
+                st.push(temp.left);
+            }
+        }
+        return ans;
+    }
+
+}
