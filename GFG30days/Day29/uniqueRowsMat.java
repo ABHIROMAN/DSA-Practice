cf123caf646bb94b7a91ec849dec55f01266d591
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2022-12-14 17:24:03 +0530

    updating unique rows

---
+++ GFG30days/Day29/uniqueRowsMat.java
@@ -0,0 +1,32 @@
+package GFG30days.Day29;
+
+import java.util.*;
+
+public class uniqueRowsMat {
+    
+    public static ArrayList<ArrayList<Integer>> uniqueRow(int a[][], int r, int c) {
+
+        HashSet<Integer> set = new HashSet<Integer>();
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+
+        int row = a.length, col = a[0].length;
+
+        for(int i = 0; i < row; i++) {
+
+            ArrayList<Integer> temp = new ArrayList<Integer>();
+            int sum = 0;
+
+            for(int j = col-1; j >= 0; j--) {
+                temp.add(a[i][j]);
+                sum += a[i][j] * Math.pow(2, col-j-1);
+            }
+
+            if(!set.contains(sum)) {
+                set.add(sum);
+                Collections.reverse(temp);
+                res.add(temp);
+            }
+        }
+        return res;
+    }
+}
