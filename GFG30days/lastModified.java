f36f95089d0a8635be410d866181dbd9104b69c8
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2023-01-09 00:37:30 +0530

    updating problem of the day

---
+++ GFG30days/lastModified.java
@@ -0,0 +1,16 @@
+package GFG30days;
+
+public class lastModified {
+    
+    int solve(int n, int[] a) {
+        // code here
+        if(a[n-1] == 9) {
+            int i = n - 1;
+            while(a[i] >= 9) {
+                i--;
+            }
+            return i + 1;
+        }
+        return n;
+    }
+}
